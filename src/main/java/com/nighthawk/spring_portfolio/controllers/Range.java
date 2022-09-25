e9fdf7151a4846c2d221094136322262d957505c
Sahil Samar <sahilsamar031@gmail.com>
2022-09-25 15:21:46 -0700

    added to range calc

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Range.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Range.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Range {
 
     @GetMapping("/range")
-    public String range(@RequestParam(name="mpg", required=true, defaultValue="World") String mpg, Model model) {
+    public String range(@RequestParam(name="mpg", required=true, defaultValue="0") int mpg, 
+    @RequestParam(name="tank", required=true, defaultValue="0") int tank, Model model) {
 
-        // model attributes are visible to Thymeleaf when HTML is "pre-processed"
         model.addAttribute("mpg", mpg);
+        model.addAttribute("tank", tank);
 
-        // load HTML VIEW (greet.html)
+        int range = mpg * tank;
+        //model.addAttribute("rangeCalc", range);
+
+        // load HTML VIEW (range.html)
         return "range"; 
 
     }
