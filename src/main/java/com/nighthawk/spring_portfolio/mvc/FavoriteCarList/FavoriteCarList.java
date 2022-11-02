51e2048efb94f36a53f8c16ee519027bbf31b695
TankeeTort <55467861+TankeeTort@users.noreply.github.com>
2022-11-01 23:51:24 -0700

    JPA attempt

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/FavoriteCarList/FavoriteCarList.java
@@ -0,0 +1,9 @@
+package com.nighthawk.spring_portfolio.mvc.FavoriteCarList;
+
+public class FavoriteCarList {
+    for (String car : carArray) {
+        if (test.size() == 0){
+            repository.save(car); //JPA save
+        }
+    }
+}
