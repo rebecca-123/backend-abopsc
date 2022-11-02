9f683cad03079ce9de8fc7fc9f4d33c6662d4aa4
Sahil Samar <sahilsamar031@gmail.com>
2022-11-02 01:39:13 -0700

    temp comment

--- src/main/java/com/nighthawk/spring_portfolio/mvc/FavoriteCarList/FavoriteCarListRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/FavoriteCarList/FavoriteCarListRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
-public interface FavoriteCarListRepository extends JpaRepository<favCar, String> {
-    // JPA has many built in methods, these few have been prototyped for this application
-    void save(String car);
+// public interface FavoriteCarListRepository extends JpaRepository<favCar, String> {
+//     // JPA has many built in methods, these few have been prototyped for this application
+//     void save(String car);
 
 
-}
+// }
