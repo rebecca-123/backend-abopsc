50e6756698fbed0ef8d74c84cb04098562b4fad7
NathanShih04 <nathanshih04@gmail.com>
2022-11-16 11:29:25 -0800

    Lists

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/FavoriteCars/FavoriteCarsList.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+import java.sql.DriverManager;
+
+public class FavoriteCarsList{
+
+    public static void main(String args[]) {
+        Connection conn = null;
+        Statement stmt = null; 
+
+        for (String cars : brandsArray) {
+            List<CarBrands> test = carsJpaRepository;
+            if (test.size() == 0)
+                repository.save(new CarBrands(null, brand, 0, 0)); //JPA save
+        }
+
+        try{
+            Class.forName("com.mysql:jbdc.Driver");
+            System.out.println("Connecting to the database");
+            conn = DriverManager;
+            .getCon
+        }
+    }
+}
