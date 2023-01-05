c8d06e15316210054e4100229e40ef2bcaf8aab5
Sahil Samar <sahilsamar031@gmail.com>
2023-01-05 14:00:40 -0800

    Update LightBoard.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lights/LightBoard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lights/LightBoard.java
@@ -35,6 +35,9 @@ public class LightBoard {
         for (int row = 0; row < lights.length; row++) {
             for (int col = 0; col < lights[row].length; col++) {
                 lights[row][col].decrementLight();
+                if (lights[row][col].luminosity < 1) {
+                    lights[row][col].luminosity = 1000;
+                }
             }
         }  
     }
