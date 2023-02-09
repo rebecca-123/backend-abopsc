9f2748a86b432de8ea825ee28e3a0689f4667af4
Sahil Samar <sahilsamar031@gmail.com>
2023-02-08 23:03:34 -0800

    Update JwtApiController.java

--- src/main/java/com/nighthawk/spring_portfolio/database/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/jwt/JwtApiController.java
@@ -38,11 +38,12 @@ public class JwtApiController {
 				.loadUserByUsername(authenticationRequest.getEmail());
 		final String token = jwtTokenUtil.generateToken(userDetails);
 		final ResponseCookie tokenCookie = ResponseCookie.from("jwt", token)
-			.httpOnly(true)
+			.httpOnly(false)
 			.secure(true)
 			.path("/")
 			.maxAge(3600)
-			.sameSite("None; Secure")
+			.secure(true)
+			.sameSite("none")
 			// .domain("example.com") // Set to backend domain
 			.build();
 		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, tokenCookie.toString()).build();
