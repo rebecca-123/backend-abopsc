480e273ad71887aa4f5dd876b7eb0be372aa552b
rohitd3 <de.rohit01@gmail.com>
2023-02-14 20:49:38 -0800

    changing delete mapping

--- src/main/java/com/nighthawk/spring_portfolio/database/car/CarApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/car/CarApiController.java
@@ -41,7 +41,7 @@ public class CarApiController {
     /*
     DELETE individual Car using ID
      */
-    @DeleteMapping("delete/{id}")
+    @getMapping("delete/{id}")
     public ResponseEntity<Object> deleteCar(@PathVariable long id) {
         repository.deleteCar(id);
         return new ResponseEntity<>( ""+ id +" deleted", HttpStatus.OK);
