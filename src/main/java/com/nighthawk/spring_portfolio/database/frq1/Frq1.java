6dd8e139879eaa1b75584043c20f65a456aec6f8
peacekeeper6 <junlim954@gmail.com>
2023-03-28 00:21:49 -0700

    comment all out for now

--- src/main/java/com/nighthawk/spring_portfolio/database/frq1/Frq1.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/frq1/Frq1.java
@@ -1,30 +1,30 @@
-package com.nighthawk.spring_portfolio.database.frq1;
-import java.util.*;
+// package com.nighthawk.spring_portfolio.database.frq1;
+// import java.util.*;
 
-public class Frq1 {
+// public class Frq1 {
   
-    ArrayList<Integer> carList = new ArrayList<Integer>();
-    /**Constructs a CarSort object that represents year.
-     * Precondition: year >= 0
-     */
+//     ArrayList<Integer> carList = new ArrayList<Integer>();
+//     /**Constructs a CarSort object that represents year.
+//      * Precondition: year >= 0
+//      */
 
-    public Frq1(int year){
-        carList = new ArrayList<Integer>();
-        if (year == 0){
-        carList.add(new Integer(0));
-        }
-        while (year > 0){
-        carList.add(0, new Integer(year % 10));
-        year /= 10;
-        }
-    } 
+//     public Frq1(int year){
+//         carList = new ArrayList<Integer>();
+//         if (year == 0){
+//         carList.add(new Integer(0));
+//         }
+//         while (year > 0){
+//         carList.add(0, new Integer(year % 10));
+//         year /= 10;
+//         }
+//     } 
 
-    public boolean isStrictlyIncreasing(){
-        for (int i = 0; i < carList.size()-1; i++){
-        if (carList.get(i).intValue() >= carList.get(i+1).intValue()){
-        return false;
-            }
-        }
-        return true;
-    }   
-}
+//     public boolean isStrictlyIncreasing(){
+//         for (int i = 0; i < carList.size()-1; i++){
+//         if (carList.get(i).intValue() >= carList.get(i+1).intValue()){
+//         return false;
+//             }
+//         }
+//         return true;
+//     }   
+// }
--- src/main/java/com/nighthawk/spring_portfolio/database/frq1/Frq1ApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/frq1/Frq1ApiController.java
@@ -1,30 +1,30 @@
-package com.nighthawk.spring_portfolio.database.frq1;
+// package com.nighthawk.spring_portfolio.database.frq1;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.*;
+// import com.fasterxml.jackson.core.JsonProcessingException;
+// import com.fasterxml.jackson.databind.JsonMappingException;
+// import com.fasterxml.jackson.databind.JsonNode;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import java.util.*;
 
-@RestController
-@RequestMapping("/api/frq1")
-public class Frq1ApiController {
+// @RestController
+// @RequestMapping("/api/frq1")
+// public class Frq1ApiController {
 
-    // private LightBoard lightBoard;
-    // private JsonNode json;
+//     // private LightBoard lightBoard;
+//     // private JsonNode json;
 
-    @GetMapping("/{values}")
-    public ResponseEntity<JsonNode> generateCarList(@PathVariable String values)
-            throws JsonMappingException, JsonProcessingException {
-        ArrayList<Integer> carList = new ArrayList<Integer>();
+//     @GetMapping("/{values}")
+//     public ResponseEntity<JsonNode> generateCarList(@PathVariable String values)
+//             throws JsonMappingException, JsonProcessingException {
+//         ArrayList<Integer> carList = new ArrayList<Integer>();
 
-        // Create objectmapper to convert String to JSON
-        ObjectMapper mapper = new ObjectMapper();
-        json = mapper.readTree(carList.toString());
+//         // Create objectmapper to convert String to JSON
+//         ObjectMapper mapper = new ObjectMapper();
+//         json = mapper.readTree(carList.toString());
 
-        return ResponseEntity.ok(json);
-    }
-}
+//         return ResponseEntity.ok(json);
+//     }
+// }
