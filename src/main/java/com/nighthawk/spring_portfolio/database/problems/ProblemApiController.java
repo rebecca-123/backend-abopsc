6b788ab4ca582970c3bc3fced0067faf2e72a5fb
Don Tran <donqt15@gmail.com>
2023-04-17 20:41:20 -0700

    problem set stuff

--- src/main/java/com/nighthawk/spring_portfolio/database/problems/ProblemApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/problems/ProblemApiController.java
@@ -14,19 +14,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nighthawk.spring_portfolio.database.grading.Assignment;
+import com.nighthawk.spring_portfolio.database.grading.AssignmentJpaRepository;
+import com.nighthawk.spring_portfolio.database.grading.Grade;
+import com.nighthawk.spring_portfolio.database.grading.GradeJpaRepository;
+import com.nighthawk.spring_portfolio.database.person.Person;
+import com.nighthawk.spring_portfolio.database.person.PersonJpaRepository;
+
 @RestController
 @RequestMapping("/api/problem")
 public class ProblemApiController {
-    
+
     @Autowired
     private ProblemJpaRepository problemJpaRepository;
 
     @Autowired
     private ProblemSetJpaRepository problemSetJpaRepository;
 
+    @Autowired
+    private AssignmentJpaRepository assignmentJpaRepository;
+
+    @Autowired
+    private PersonJpaRepository personJpaRepository;
+
+    @Autowired
+    private GradeJpaRepository gradeJpaRepository;
 
     // Okay basically I'm going to be returning a Map<String, Map<String, Boolean>>
-    //                                              question,     answer, correctness
+    // question, answer, correctness
     @PostMapping("/getProblemSetMC")
     public ResponseEntity<Object> getProblemSetMC(@RequestBody final Map<String, Object> map) {
         Optional<ProblemSet> optionalProblemSet = problemSetJpaRepository.findById((Long) map.get("id"));
@@ -56,7 +71,7 @@ public class ProblemApiController {
 
             mc.put(question, answers);
         }
-        
+
         return new ResponseEntity<>(mc, HttpStatus.OK);
     }
 
@@ -78,7 +93,45 @@ public class ProblemApiController {
         Map<String, Object> resp = new HashMap<>();
         resp.put("id", problemSet.getId());
         return new ResponseEntity(resp, HttpStatus.OK);
-       
+
     }
 
+    @PostMapping("/score")
+    public ResponseEntity<Object> score(@RequestBody final Map<String, Object> map) {
+        String problemSetName = (String) map.get("problemSet");
+        String scoreString = (String) map.get("score");
+        String userIDString = (String) map.get("userID");
+
+        double score = Double.valueOf(scoreString);
+        long userID = Long.valueOf(userIDString);
+
+        ProblemSet problemSet = problemSetJpaRepository.findByName(problemSetName);
+        Person person = personJpaRepository.findById(userID).orElse(null);
+
+        if (problemSet == null || person == null) {
+            return new ResponseEntity<>("problem set/person doesn't exist", HttpStatus.BAD_REQUEST);
+        }
+
+        Assignment assignment = problemSet.getAssignment();
+
+        if (problemSet == null) {
+            return new ResponseEntity<>("assignment don't exist, idk how this error would ever occur but ok lmao",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        Grade grade = gradeJpaRepository.findByPersonAndAssignment(person, assignment);
+
+        if (grade == null) {
+            return new ResponseEntity<>("grade doesn't exist", HttpStatus.BAD_REQUEST);
+        }
+
+        double gradeScaleFactor = 1;
+
+        grade.setGrade(score / gradeScaleFactor);
+        grade.setComment("quiz completed and graded");
+
+        gradeJpaRepository.save(grade);
+
+        return new ResponseEntity<>("score submitted", HttpStatus.OK);
+    }
 }
