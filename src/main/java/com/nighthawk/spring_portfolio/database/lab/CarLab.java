5b9bde2544c7365bad6359a7dc4321542b79ff44
Rebecca-123 <rebecca.notw@gmail.com>
2023-04-27 09:37:44 -0700

    extra credit

--- src/main/java/com/nighthawk/spring_portfolio/database/lab/CarLab.java
+++ src/main/java/com/nighthawk/spring_portfolio/database/lab/CarLab.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,32 +16,65 @@ public class CarLab { // CarLab is a class that represents a car
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(unique=true)
+    @Column(unique = true)
     private String name;
 
-    // Hack: add columns for other attributes, (ex total mileage, model, make of car) demonstrating knowledge of different data types
-
-    @Column()
-    private double price;
-    
     public CarLab(String name, double price) {
         this.name = name;
         this.price = price;
-        
+
+    }
+
+    public CarLab(String name, double price, int mileage, int gallons, double mpg, String model, String make) {
+        this.name = name;
+        this.price = price;
+        this.mileage = mileage;
+        this.gallons = gallons;
+        this.mpg = mpg;
+        this.model = model;
+        this.make = make;
+
+    }
+
+    @Column()
+    private double price;
+
+    // Hack: add columns for other attributes, (ex total mileage, model, make of
+    // car) demonstrating knowledge of different data types
+    @Column()
+    private int mileage;
+
+    @Column()
+    private int gallons;
+
+    @Column()
+    private double mpg;
+
+    @Column()
+    private String model;
+
+    @Column()
+    private String make;
+
+    public static void calcMpg(String car1Name, double car1Price) {
+        CarLab car1 = new CarLab(car1Name, car1Price);
+
+        car1.setMpg((double) car1.getMileage() / car1.getGallons());
     }
 
     public static String cheaperCar(String car1Name, double car1Price, String car2Name, double car2Price) {
         CarLab car1 = new CarLab(car1Name, car1Price);
         CarLab car2 = new CarLab(car2Name, car2Price);
-        
+
         if (car1.getPrice() < car2.getPrice()) {
             return car1.getName();
         } else {
             return car2.getName();
         }
     }
-    
-    public static String cheaperCar(CarLab car1, CarLab car2) { // Overloading the cheaperCar method to allow both String and CarLab objects to be passed in
+
+    public static String cheaperCar(CarLab car1, CarLab car2) { // Overloading the cheaperCar method to allow both
+                                                                // String and CarLab objects to be passed in
         if (car1.getPrice() < car2.getPrice()) {
             return car1.getName();
         } else {
@@ -57,10 +89,10 @@ public class CarLab { // CarLab is a class that represents a car
     }
 
     public String toString() {
-        return( "{" + 
-            "\"name\": " + name + "," +
-            "\"price\": " +  price +
-            "}" );
+        return ("{" +
+                "\"name\": " + name + "," +
+                "\"price\": " + price +
+                "}");
     }
 
     public static void main(String[] args) {
